a0d12fca73635c07f4ed6fa635ee9103480d1fc4
Carlo Namoca <carlo.namoca@gmail.com>
2024-04-09 10:03:22 -0600

    Added new gestures and buttons to manipulate BPM

---
+++ bpm-buddy/AnimatingCircle.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatingCircle.swift
+//  bpm-buddy
+//
+//  Created by Carlo Namoca on 2024-04-09.
+//
+
+import SwiftUI
+
+struct AnimatingCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatingCircle()
+}
