0c31d2431e581521fe38dcc44dc4a4d503db17bf
Carlo Namoca <carlo.namoca@gmail.com>
2024-04-09 11:05:27 -0600

    Added base code for metronome and colour themes

---
+++ bpm-buddy/ColourTheme.swift
@@ -0,0 +1,8 @@
+//
+//  ColourTheme.swift
+//  bpm-buddy
+//
+//  Created by Carlo Namoca on 2024-04-09.
+//
+
+import Foundation
---
+++ bpm-buddy/MetronomeManager.swift
@@ -0,0 +1,8 @@
+//
+//  MetronomeManager.swift
+//  bpm-buddy
+//
+//  Created by Carlo Namoca on 2024-04-09.
+//
+
+import Foundation
